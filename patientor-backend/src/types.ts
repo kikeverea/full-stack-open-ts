50b1bb776de5e5844157932603e52d95148e4dab
Enrique Verea <kikeverea@gmail.com>
2023-02-13 23:05:32 +0100

    9.23 Backend, define type union Entry

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnose {
+export interface Diagnosis {
   code: string,
   name: string,
   latin?: string
@@ -8,11 +8,6 @@ export enum Gender {
   Male = 'male',
   Female = 'female',
   Other = 'other'
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-
 }
 
 export interface Patient {
@@ -25,6 +20,48 @@ export interface Patient {
   entries: Entry[]
 }
 
+interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck',
+  healthCheckRating: HealthCheckRating;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare'
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  },
+}
+
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
